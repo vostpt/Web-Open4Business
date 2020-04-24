a69f4b34bb26c0e4ce8fb48f00deac9f1991cf71
Pedro Santos <pedro.santos@domatica.pt>
2020-04-24 16:44:24 +0100

    improve exception handling for 401 error

--- src/core-modules/core/interceptors/api-in.interceptor.ts
+++ src/core-modules/core/interceptors/api-in.interceptor.ts
@@ -1,9 +1,9 @@
 import { Injectable, Injector } from '@angular/core';
-import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpResponse } from '@angular/common/http';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpResponse, HttpErrorResponse } from '@angular/common/http';
 import { Router } from '@angular/router';
 
-import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
+import { tap, retry, catchError } from 'rxjs/operators';
 
 import { environment } from '../models/environment.model';
 
@@ -21,6 +21,44 @@ export class ApiInInterceptor implements HttpInterceptor {
     this.router = this.injector.get(Router);
 
     return next.handle(request).pipe(
+      retry(1),
+      catchError((error: HttpErrorResponse) => {
+        let errorMessage = '';
+        if (error.error instanceof ErrorEvent) {
+          // client-side error
+          errorMessage = `Error: ${error.error.message}`;
+        } else {
+          // server-side error
+          errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+        }
+        console.error('API ERROR', error.status, errorMessage);
+
+        switch (error.status) {
+          case 400: // Bad request!
+            // TODO
+            break;
+          case 401:
+            localStorage.removeItem('token');
+            localStorage.removeItem('email');
+            localStorage.removeItem('name');
+            localStorage.removeItem('isA');
+            this.router.navigate(['/auth/signin']);
+            break;
+          case 403: // Forbidden.
+            this.router.navigate(['/forbidden']);
+            break;
+          case 404: // Not found!
+            // TODO
+            break;
+          case 500: // Internal Server Error!
+            // TODO
+            break;
+          default:
+            break;
+        }
+
+        return throwError(errorMessage);
+      }),
       tap((event: HttpEvent<any>) => {
         if (event instanceof HttpResponse) {
           switch (event.status) {
@@ -29,7 +67,10 @@ export class ApiInInterceptor implements HttpInterceptor {
               break;
             case 401:
               localStorage.removeItem('token');
-              // window.location.href = `${environment.accountsUrl}signin`;
+              localStorage.removeItem('email');
+              localStorage.removeItem('name');
+              localStorage.removeItem('isA');
+              this.router.navigate(['/auth/signin']);
               break;
             case 403: // Forbidden.
               this.router.navigate(['/forbidden']);
